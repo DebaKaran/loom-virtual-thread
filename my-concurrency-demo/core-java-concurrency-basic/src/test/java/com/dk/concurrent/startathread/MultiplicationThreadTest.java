4dee01c0116b96127074662d84942dbd601756d3
DebaKaran <karan.deba@gmail.com>
2025-08-01 21:13:31 +0530

    Checking number of thread is equals to different tables

--- my-concurrency-demo/core-java-concurrency-basic/src/test/java/com/dk/concurrent/startathread/MultiplicationThreadTest.java
+++ my-concurrency-demo/core-java-concurrency-basic/src/test/java/com/dk/concurrent/startathread/MultiplicationThreadTest.java
@@ -21,5 +21,14 @@ public class MultiplicationThreadTest {
 
         assertTrue(result.stream().allMatch(line -> line.startsWith("Thread-")));
         assertTrue(result.stream().allMatch((line -> line.contains(" = "))));
+
+        //Check that threads were actually parallel (rough check via thread names):
+        long distinctThreads = result.stream()
+                .map(line -> line.split(":")[0])
+                .distinct()
+                .count();
+        assertTrue(distinctThreads > 1 && distinctThreads == numberOfTables,
+                "Should involve multiple threads");
+
     }
 }
